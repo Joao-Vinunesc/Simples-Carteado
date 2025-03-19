28a96332e9ea5b1ec42c81ac1aef824ca82f1b37
João <Joao.vinunesc@gmail.com>
2025-03-19 12:05:56 -0300

    update - refatoração de codigo e começo de estudo sobre visuais (tkinter)

--- Mod/Jogador.py
+++ Mod/Jogador.py
@@ -1,3 +1,4 @@
+import random
 class Jogador():
     def __init__(self,  deck):
         self.gemas = 0
@@ -13,3 +14,35 @@ class Jogador():
             deck.append(carta.nome)
 
         return f'Vida{self.vida},\n Mão:{self.mao}\n deck:{deck}'
+
+    
+    def mao_inicial(self, inicio):
+        mao=[]
+        for item in range(inicio):
+            carta=random.choice(self.deck)
+            mao.append(carta)            
+            self.deck.remove(carta)
+        return mao
+
+    def fase_compra(self):
+        carta = random.choice(self.deck)
+        self.mao.append(carta)
+        self.deck.remove(carta)
+    
+    #--Necessario iniciar a função com a entrada da carta escolhida da mão!
+    def invocar_no_campo(self,carta):
+        self.mao.remove(carta)
+        self.campo.append(carta)
+    
+    #--Seleciona as carta dentro do CAMPO para o ataque | Retorna lista com as cartas para o ataque
+    def selecionar_ataque(self):
+        grupo_ataque=[]
+        for carta in self.campo:
+            input(f'Colocar {carta.nome}|{carta.poder}|{carta.vida} no grupo de ataque?')
+            #--Colocar futuramente uma condicional aqui para selecionar quais cartas serão adicionadas
+            grupo_ataque.append(carta)
+        return(grupo_ataque)
+
+
+
+
